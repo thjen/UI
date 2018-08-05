9d350c87ec4f7894ad9435296061f8eb4081c2a2
qThjen <thjenxxxno6@gmail.com>
2018-08-05 17:21:10 +0700

    Initial commit

---
+++ app/src/main/java/qthjen_dev/io/uiandux/MainActivity.java
@@ -0,0 +1,22 @@
+package qthjen_dev.io.uiandux;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = findViewById(R.id.button3);
+    }
+}
